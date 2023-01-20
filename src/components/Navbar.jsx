f05dae6ef4585b37e001ce0e418f4c45f0a83d96
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-20 19:12:34 +0530

    completed navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Navbar (){
+    return (
+        <nav className="nav">
+            <img src="src/assets/Vector.png" alt="Air BnB Logo" className="nav--logo" />
+        </nav>
+    );
+}
