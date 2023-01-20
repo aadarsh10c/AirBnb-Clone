269977871c0e220b0931fb23d16f847b46586e14
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-20 23:02:44 +0530

    Hero componenet completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Hero () {
+    return (
+        <div className="hero">
+            <div className="hero--img">
+                <img src="src/assets/photo-grid.png" alt="" className="hero--img-grid" />
+            </div>
+            <div className="hero--text">
+                <h2>
+                    Online Expieriences
+                </h2>
+                <p>
+                    Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                </p>
+            </div>
+        </div>
+    );
+}
